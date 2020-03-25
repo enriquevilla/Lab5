c06eea093fe7a12fe5d14406dae9f30b66be8d6d
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-25 10:48:36 -0600

    Added event delegation

--- js/index.js
+++ js/index.js
@@ -2,17 +2,6 @@ function createItem(value) {
     let newItem = document.createElement("li");
     let itemText = document.createElement("p");
     itemText.innerText = value;
-    newItem.addEventListener("click", (event) => {
-        if (event.target.matches(".check")) {
-            if (newItem.classList.contains("checked")) {
-                newItem.classList.remove("checked");
-            } else {
-                newItem.classList.add("checked");
-            }
-        } else if (event.target.matches(".delete")) {
-            newItem.remove();
-        }
-    });
 
     let buttonDiv = document.createElement("div");
     buttonDiv.appendChild(addCheck());
@@ -45,6 +34,17 @@ function setEvent() {
         if (document.querySelector("input") != "") {
             createItem(document.querySelector("input").value);
             document.querySelector("input").value = "";
+        }
+    });
+    document.querySelector("ul").addEventListener("click", (event) => {
+        if (event.target.matches(".check")) {
+            if (event.target.parentNode.parentNode.classList.contains("checked")) {
+                event.target.parentNode.parentNode.classList.remove("checked");
+            } else {
+                event.target.parentNode.parentNode.classList.add("checked");
+            }
+        } else if (event.target.matches(".delete")) {
+            event.target.parentNode.parentNode.remove();
         }
     });
 }
